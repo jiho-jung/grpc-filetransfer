ec49f40cf0d7747cecf95aaa8850414d370e301c
JihoJung <jiho.jung@joyent.com>
2025-04-30 14:06:03 +0900

    Fix the script

--- run_test.sh
+++ run_test.sh
@@ -3,12 +3,12 @@
 file="1GB.bin"
 
 echo "### NLB Test"
-./client -a 37.153.116.128:443 -t true -f ./$file  -b 1600
+./client -a 37.153.116.128:443 -t true -f ./$file  -b 1600 -n NLB
 
 echo "### Target Test"
-./client -a 37.153.116.251:9000 -t true -f ./$file  -b 1600 
+./client -a 37.153.116.251:9000 -t true -f ./$file  -b 1600 -n Target
 
 echo "### CLB Test"
-./client -a 37.153.116.43:443 -t true -f ./$file  -b 1600
+./client -a 37.153.116.43:443 -t true -f ./$file  -b 1600 -n CLB
 
 

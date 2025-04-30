f5c3f2efbf75dffc363d2707f784db5d522bfb5f
JihoJung <jiho.jung@joyent.com>
2025-04-30 15:00:11 +0900

    Add test round

--- run_test.sh
+++ run_test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 file="1GB.bin"
+MAX=5
 
-echo "### NLB Test"
-./client -a 37.153.116.128:443 -t true -f ./$file  -b 1600 -n NLB
+for (( i=0; i<MAX; i++ )) ; {
+	echo "#### Round $i ###"
 
-echo "### Target Test"
-./client -a 37.153.116.251:9000 -t true -f ./$file  -b 1600 -n Target
+	echo "### Round $i: NLB Test"
+	./client -a 37.153.116.128:443 -t true -f ./$file  -b 1600 -n NLB
 
-echo "### CLB Test"
-./client -a 37.153.116.43:443 -t true -f ./$file  -b 1600 -n CLB
+	echo "### Round $i: Target Test"
+	./client -a 37.153.116.251:9000 -t true -f ./$file  -b 1600 -n Target
 
+	echo "### Round $i: CLB Test"
+	./client -a 37.153.116.43:443 -t true -f ./$file  -b 1600 -n CLB
+}
 
